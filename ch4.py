cd7de83c6494682933c2c001dfb57056ab35cd4d
Mike <munkyu0813@gmail.com>
2017-07-10 19:40:43 -0400

    from week 2 reading

---
+++ ch4.py
@@ -0,0 +1,75 @@
+print(len('Albert Einstein'))
+
+print(len(' '))
+
+print(len(''))
+
+print('Albert' + ' Einstein')
+
+# Using str() to get a string representation of a number
+'Four score and ' + str(7) + ' years ago'
+
+# Using int() to get an integer representation of a string
+int('11')
+
+# Using float() to get a float representation of a number
+float('-324')
+
+# Repeating string
+'AT' * 5
+
+4 * '-'
+
+# yileds empty string if integer is less than or equal to 0
+'GC' * 0
+
+'TA' * -3
+
+# Assigning string to a variable
+sequence = 'ATTGTCCCCC'
+len(sequence)
+
+new_sequence = sequence + 'GGCCTCCTGC'
+
+new_sequence
+
+new_sequence * 2
+
+# escape characters
+'She said, "That' + "'" + 's hard to read."'
+
+len('\'')
+
+'''one
+two
+three'''
+
+# print()
+print('one\ttwo\nthree\tfourr')
+
+print(1, 'two', 'three', 4.0)
+
+radius = 5
+print('The diameter of the circle is', radius * 2, 'cm.')
+
+print('a', 'b', 'c')
+
+print('a', 'b', 'c', sep=', ') # sep= is space by default
+
+print('a', 'b', 'c', sep=', ', end='') # end= is newline by default
+
+print('a', 'b', 'c', end=' and the end.\n')
+
+# Getting info from the keyboard
+species = input() #  returns whatever entered as a string
+
+type(species)
+
+population = int(input()) # convert input to integer
+
+type(population)
+
+name = input("Please enter your name: ")
+print(name)
+
+
